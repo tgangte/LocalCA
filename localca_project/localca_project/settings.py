f663ecb5b750bf33a5392263f02d2cf74b9fec7d
chrisrowley14 <chrisrowley14@users.noreply.github.com>
2025-04-19 20:07:26 +0100

    Added CSRF_TRUSTED_ORIGINS

--- localca_project/localca_project/settings.py
+++ localca_project/localca_project/settings.py
@@ -135,3 +135,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'homepage'  # Where to redirect after successful login
 LOGOUT_REDIRECT_URL = 'homepage'  # Where to redirect after logout
 LOGIN_URL = 'login'  # Where to redirect if login is required
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS').split(",")
